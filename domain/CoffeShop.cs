e2a5df1f189d94ca865e705673f626e4b9ad2394
h.motallebi <motallebi.hassan@gmail.com>
2024-06-16 14:02:09 +0200

    Rename the Shop to the CoffeeShop and added ServeList to the Shop

--- domain/CoffeShop.cs
+++ domain/CoffeShop.cs
@@ -1,7 +1,8 @@
-public class Shops
+public class CoffeShop
 {
     public int Id {get; set;}
     public string Name { get; set; }
     public string Address { get; set; }
-    
+    public List<Coffee> ServeList { get; set; }
+        
 }
